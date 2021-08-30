74f3c6487566d82a65338a7bf77c4aa7abd60b66
beom324 <81265122+beom324@users.noreply.github.com>
2021-08-30 22:34:45 +0900

    qna 답장부분 미완

--- src/main/java/gp/domain/QnaRepository.java
+++ src/main/java/gp/domain/QnaRepository.java
@@ -1,6 +1,11 @@
 package gp.domain;
 
+import gp.web.dto.QnaDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface QnaRepository extends JpaRepository<QnaEntity, Long> {
+
 }
