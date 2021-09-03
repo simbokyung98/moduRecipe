b5940fed5d2b7aec1ed5387c2bd7d979dbf1dd08
beom324 <81265122+beom324@users.noreply.github.com>
2021-09-03 14:27:58 +0900

    gitignore

--- src/main/java/gp/domain/QnaRepository.java
+++ src/main/java/gp/domain/QnaRepository.java
@@ -2,21 +2,6 @@ package gp.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.transaction.Transactional;
-import java.util.List;
-
 public interface QnaRepository extends JpaRepository<QnaEntity, Long> {
 
-    @Modifying
-    @Query(value = "select b from QnaEntity b where answerstate =1")
-    List<QnaEntity> find();
-
-    @Query( "UPDATE QnaEntity SET answerstate=1 WHERE answercontent is not null")
-    List<QnaEntity> updateAnswerState();
-
-    @Query(value = "select b from QnaEntity b where answerstate =0")
-    List<QnaEntity> nullfind();
 }
