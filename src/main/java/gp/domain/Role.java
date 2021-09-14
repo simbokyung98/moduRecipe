12e1c062d18c27725c8d99ef286e5fe0885a8f64
kimsukyoung <rlatnrud0714@naver.com>
2021-09-14 21:42:01 +0900

    로그인

---
+++ src/main/java/gp/domain/Role.java
@@ -0,0 +1,13 @@
+package gp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Role {
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    private String value;
+}
