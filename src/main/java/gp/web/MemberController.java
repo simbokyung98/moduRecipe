9f03f0d99a70e72be6ffc0fd88c8c33e6ee19a1f
simbokyung <synk98@gmail.com>
2021-09-29 00:43:46 +0900

    로그인 구현

--- src/main/java/gp/web/MemberController.java
+++ src/main/java/gp/web/MemberController.java
@@ -1,5 +1,6 @@
 package gp.web;
 
+import gp.domain.Member;
 import gp.service.MemberService;
 import gp.web.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +8,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final HttpServletRequest request;
+    private final HttpSession session;
 
 
     @GetMapping("/")
     public String main() {
-        return "/main";
+        return "/index";
     }
 
     // 회원가입
@@ -36,6 +42,20 @@ public class MemberController {
     @GetMapping("/login")
     public String login() {
         return "/memberLogin";
+    }
+
+    @PostMapping("/userLogin")
+    public String userLogin(MemberDto memberDto){
+        MemberDto result = memberService.userLogin(memberDto);
+
+        if(result != null){
+
+            session.setAttribute("user", result);
+            return "redirect:/" ;
+
+        }else{
+            return "redirect:/login";
+        }
     }
 
 }
